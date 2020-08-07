72a77e7541bd556edede64fd200371d60224e76f
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-07 09:50:18 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,41 @@
+//Step-1
+//Output will be undefined
+//------------------------
+// let name;
+// console.log(name)
+
+//Step-2
+//Output will be undefined
+//------------------------
+// function add(num1, num2)
+// {
+//     console.log(num1 + num2);
+// }
+// const result = add(13,15);
+// console.log(result);
+
+//Step-3
+//Output will be undefined
+//------------------------
+// function add(num1, num2)
+// {
+//     return;
+// }
+// const result = add(13,15);
+// console.log(result);
+
+//Step-4
+//Output will be undefined
+//------------------------
+// function add(num1, num2)
+// {
+//     console.log(num1,num2)
+// }
+// const result = add(13);
+// console.log(result);
+
+//Step-5
+//Output will be undefined
+//------------------------
+const object = {name: "Ariful Islam", roll: 171966}
+console.log(object.department);
