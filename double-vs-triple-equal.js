3fb88c55ed25d570a7bcdd8c284df481f24c1b49
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-07 10:03:28 +0600

    double vs triple equal problem solved

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,35 @@
+//Output will be True
+// const first = 2;
+// // const second = "2";
+// const second = 2;
+// if(first === second)
+// {
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is False");
+// }
+
+
+// //Output will be True
+// const first = 1;
+// const second = true;
+// if(first == second)
+// {
+//     console.log("Condition is True");
+// }
+// else{
+//     console.log("Condition is False");
+// }
+
+
+//Output will be False
+const first = 1;
+const second = false;
+if(first === second)
+{
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
