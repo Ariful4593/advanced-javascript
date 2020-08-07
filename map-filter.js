1cd50eec0dd487707760101a8fca43e4568a7d39
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-07 10:44:08 +0600

    map filer

---
+++ map-filter.js
@@ -0,0 +1,23 @@
+const numbers = [3,4,5,6];
+// const output = [];
+// for(var i=0;i<numbers.length;i++)
+// {
+//     const element = numbers[i];
+//     const result = element*element;
+//     output.push(result)
+// }
+
+
+
+// function square(element){
+//     return element*element;
+// }
+// const result = numbers.map(square)
+// console.log(result);
+
+// const result = numbers.map(x => x * x)
+// console.log(result)
+
+//Filter Area
+const bigger = numbers.filter(x => x > 3);
+console.log(bigger);
