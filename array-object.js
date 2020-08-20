7ebb3628e1ad8390ccc1a86fd147814b614d19d6
Ariful4593 <arifkhagoria2015@gmail.com>
2020-08-20 20:20:38 +0600

    Array Object Accessing

---
+++ array-object.js
@@ -0,0 +1,9 @@
+const student = [
+    {id: 01,name: "Ariful ISLAM"},
+    {id: 02,name: "Shoriful Islam"},
+    {id: 03,name: "Shofiq Ahmed"}
+]
+// console.log(student)
+
+const result = student.map(x => x.name);
+console.log(result)
--- map-filter-advanced.js
+++ map-filter-advanced.js
@@ -13,3 +13,18 @@ const output = []
 const names = student.map(s => s.name)
 
 console.log(names)
+
+
+const songBtn = document.getElementById("search-btn");
+songBtn.addEventListener("click", function(){
+    const searchItem = document.getElementById("search-song").value;
+    fetch("https://api.lyrics.ovh/suggest/baby")
+    .then(res => res.json())
+    .then(data => {
+        const title = data.map(s => s.title);
+        console.log(title)
+    })
+    console.log(searchItem)
+})
+const title = data.data
+const name = title.map(s => s.album.title)
